137108c027068d8a56606395f9b454bc80cc66b5
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-07-03 17:29:33 +0200

    written te insert job logic

--- server.js
+++ server.js
@@ -13,11 +13,15 @@ App.post("/comments", async (req, res) => {
   try {
     const client = await pool.connect();
     await client.query("BEGIN");
-    await client.query("INSERT INTO comments(userId,text) VALUES($1,$2", [
-      userId,
-      text,
-    ]);
-    res.status(201).json({ message: "text sent successifull" });
+    const result = await client.query(
+      "INSERT INTO comments(user_id,text) VALUES($1,$2) RETURNING id",
+      [userId, text],
+    );
+
+    const commentId = result.rows[0].id;
+    res
+      .status(201)
+      .json({ message: "text sent successfully                       " });
     console.log(`UserId: ${userId} comment ${text}`);
   } catch (err) {
     res.status(500).json({ message: "error while sending text", error: err });
