72aaa0d9ae61bff276af1a2bde078dd021e66da7
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-07-03 18:32:14 +0200

    added the rollback and relaese of the client

--- server.js
+++ server.js
@@ -9,22 +9,31 @@ const App = express();
 App.use(express.json());
 
 App.post("/comments", async (req, res) => {
+  let client;
   const { userId, text } = req.body;
   try {
-    const client = await pool.connect();
+    client = await pool.connect();
     await client.query("BEGIN");
     const result = await client.query(
-      "INSERT INTO comments(user_id,text) VALUES($1,$2) RETURNING id",
+      "INSERT INTO comments(userId,Text) VALUES($1,$2) RETURNING id",
       [userId, text],
     );
-
     const commentId = result.rows[0].id;
+    await client.query(
+      "INSERT INTO jobs(event_type,payload,status) VALUES($1,$2,$3)",
+      ["comment.created", { commentId, userId }, "pending"],
+    );
+    await client.query("COMMIT");
+
     res
       .status(201)
       .json({ message: "text sent successfully                       " });
     console.log(`UserId: ${userId} comment ${text}`);
   } catch (err) {
+    await client.query("ROLLBACK");
     res.status(500).json({ message: "error while sending text", error: err });
+  } finally {
+    if (client) client.release();
   }
 });
 
